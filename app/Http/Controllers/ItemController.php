a8bf2add0948c56a6679d61e4ad7921f59f7dbcd
Arakawa Shintaro <arashinbe@gmail.com>
2017-08-26 22:52:13 +0900

    define validation of content

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -35,7 +35,7 @@ class ItemController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(ItemStoreFormRequest $request)
     {
         $item = new Item();
         // todo: ログインユーザのidが入るようにする
