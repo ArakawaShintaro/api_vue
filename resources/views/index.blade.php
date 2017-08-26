2569a1c90b6ae6377e4a4e3ce429de04c99d8a3b
Arakawa Shintaro <arashinbe@gmail.com>
2017-08-27 01:09:06 +0900

    create index page

---
+++ resources/views/index.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>TODO app with Laravel + Vue</title>
+    <link rel="stylesheet" href="css/app.css"/>
+    <script type="text/javascript">
+        window.Laravel = window.Laravel || {};
+        window.Laravel.csrfToken = "{{csrf_token()}}";
+    </script>
+</head>
+<body>
+<div id="app">
+</div>
+<script src="js/app.js"></script>
+</body>
+</html>
