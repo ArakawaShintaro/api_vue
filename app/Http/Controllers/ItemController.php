8ce1ba9c97002106d5676623a5968bce5d2163bd
Arakawa Shintaro <arashinbe@gmail.com>
2017-08-26 22:51:45 +0900

    add ItemController#store

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -37,7 +37,12 @@ class ItemController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $item = new Item();
+        // todo: ログインユーザのidが入るようにする
+        $item->user_id = \App\User::query()->first()->id;
+        $item->content = $request->input('content');
+        $item->save();
+        return response($item, 201);
     }
 
     /**
