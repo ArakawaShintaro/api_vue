cbcce910290cbc744d9fe3cb8c21320af6e194c6
Arakawa Shintaro <arashinbe@gmail.com>
2017-08-26 22:00:34 +0900

    create seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use \Faker\Factory;
+use \App\User;
+use \App\Item;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +14,18 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $faker = Factory::create();
+
+        $user = new User();
+        $user->name = $faker->name;
+        $user->email = $faker->unique()->safeEmail;
+        $user->password = bcrypt('password');
+        $user->remember_token = str_random(10);
+        $user->save();
+
+        $item = new Item();
+        $item->user_id = $user->id;
+        $item->content = $faker->text;
+        $item->save();
     }
 }
